48c3a410b39f9521247a4e286d4911151e82f34c
OnaPath <103204959+OnaPath@users.noreply.github.com>
2025-03-03 10:43:37 -0700

    Updated Pricing

--- assets/js/deliveryForm.js
+++ assets/js/deliveryForm.js
@@ -39,19 +39,19 @@ if (!window.DeliveryFormHandler) {
         this.paymentConfig = {
             airdrie: {
                 city: 'airdrie',
-                deliveryFee: 19.00,
+                deliveryFee: 19.75,
                 distanceSurcharge: 0,
                 gstRate: 0.05
             },
             calgary: {
                 city: 'calgary',
-                deliveryFee: 20.00,
+                deliveryFee: 24.75,
                 distanceSurcharge: 5.00,
                 gstRate: 0.05
             },
             lethbridge: {
                 city: 'lethbridge',
-                deliveryFee: 19.00,
+                deliveryFee: 19.75,
                 distanceSurcharge: 0,
                 gstRate: 0.05
             }
